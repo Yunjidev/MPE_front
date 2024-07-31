0121562c3d6d80e1e2434fd149a0dc82667e9f04
BrvAlexis <brevieralexis@gmail.com>
2024-07-31 10:36:32 +0200

    daysi

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { HomeIcon } from '@heroicons/react/24/solid'
+
 
 export default function NavBar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-    </nav>
+    <>
+      <div className="navbar bg-base-100 px-2 sm:px-4 py-2.5 rounded">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <div className="form-control mr-3">
+            <input type="text" placeholder="Search" className="input input-bordered" />
+          </div>
+          <button className="btn btn-ghost btn-circle" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            <HomeIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+          {isDropdownOpen && (
+            <div className="dropdown dropdown-end">
+              <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Profile</a></li>
+                <li><a>Settings</a></li>
+                <li><a>Logout</a></li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      {isDropdownOpen && (
+        <div className="fixed inset-0 z-10 bg-black bg-opacity-25" onClick={() => setIsDropdownOpen(false)}></div>
+      )}
+    </>
   );
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,6 @@ module.exports = {
   },
   plugins: [
     require('flowbite/plugin'),
+    require('daisyui'),
   ],
 }
