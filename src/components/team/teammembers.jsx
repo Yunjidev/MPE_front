db7ca326d0b92edd55180e0c06385a7903351d04
MacDuPain <godel.Sacha@gmail.com>
2024-07-31 15:46:45 +0200

    réinstallation tailwind

--- src/components/team/teammembers.jsx
+++ src/components/team/teammembers.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TeamMember = ({ name, github, linkedin, avatar }) => {
   return (
     <div className="text-center">
-      <img className="inline-block w-[62px] h-[62px] rounded-full" src={avatar} alt={`${name}'s avatar`} />
+      <img className="inline-block w-[62px] h-[62px] rounded-3xl" src={avatar} alt={`${name}'s avatar`} />
       <h3 className="mt-2 font-semibold">{name}</h3>
       <div className="flex justify-center space-x-2 mt-1">
         {github && <a href={github} className="text-gray-600 hover:text-gray-800"><i className="fab fa-github"></i></a>}
