e4185d980cb8b2b83a07d9f22c48bfd7d42dabc1
lexi <alexiacabanel1@gmail.com>
2024-07-31 18:30:01 +0200

    modif emplacement pricing et reroutage

--- src/app.jsx
+++ src/app.jsx
@@ -3,19 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import UserChoiceModal from "./components/home/UserChoiceModal";
+import Home from "./pages/home/home";
 import HomeClient from "./pages/home/HomeClient";
 import HomeEnterprise from "./pages/home/HomeEntreprise";
 import Signup from "./components/user/signup";
 import Signin from "./components/user/signin";
 import SocialLinks from "./components/SocialLinks/sociallinks";
-import Pricing from "./components/pricing/pricing";
 
 
 function MainLayout({ children }) {
   return (
     <>
       <NavBar />
-      <Pricing />
       <SocialLinks />
       <main>{children}</main>
       <Footer />
@@ -28,6 +27,7 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<UserChoiceModal />} />
+      <Route path="/home" element={<Home />} />
         <Route path="/home-client" element={<HomeClient />} />
         <Route path="/home-enterprise" element={<HomeEnterprise />} />
         <Route
