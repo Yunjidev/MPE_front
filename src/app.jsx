31c0d17998dbc1bb9d6cd6015e7e7d902b551555
lexi <alexiacabanel1@gmail.com>
2024-08-01 10:02:15 +0200

    added particles background

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ParticlesDemo from "./components/ParticlesDemo";
 import NavBar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import UserChoiceModal from "./components/home/UserChoiceModal";
@@ -10,48 +11,37 @@ import Signup from "./components/user/signup";
 import Signin from "./components/user/signin";
 import SocialLinks from "./components/SocialLinks/sociallinks";
 
-
-function MainLayout({ children }) {
+const Layout = ({ children }) => {
   return (
-    <>
+    <div className="relative min-h-screen bg-black text-white">
       <NavBar />
       <SocialLinks />
-      <main>{children}</main>
+      <ParticlesDemo />
+      <main className="relative z-10">{children}</main>
       <Footer />
-    </>
+      
+    </div>
   );
-}
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<UserChoiceModal />} />
-      <Route path="/home" element={<Home />} />
-        <Route path="/home-client" element={<HomeClient />} />
-        <Route path="/home-enterprise" element={<HomeEnterprise />} />
-        <Route
-          path="/signup"
-          element={
-            <MainLayout>
-              <Signup />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/signin"
-          element={
-            <MainLayout>
-              <Signin />
-            </MainLayout>
-          }
-        />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<UserChoiceModal />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home-client" element={<HomeClient />} />
+          <Route path="/home-enterprise" element={<HomeEnterprise />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
 
-MainLayout.propTypes = {
+Layout.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
