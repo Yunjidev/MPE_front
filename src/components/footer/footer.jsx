5f6509b32aafb759e2a6e90c3e4c3478123fcfed
MacDuPain <godel.Sacha@gmail.com>
2024-08-02 12:51:59 +0200

    affichage du footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,12 @@
-export default function Footer() {
-    return <p>Footer</p>
-}
+import React from 'react';
+import './footer.css'; // Import du fichier CSS
+
+const Footer = () => {
+  return (
+    <footer className="footer flex justify-center">
+      <p>© 2024 My Website. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
