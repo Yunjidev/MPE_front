931d1538b6339689f495d8cd266387aa08d94e7f
Yunjidev <florian.vancamp@gmail.com>
2024-08-02 15:49:49 +0200

    Ajout du switch une fois connecter, Dashboad et Deconnexion + Avatar mais à lié bdd

--- src/components/user/signup.jsx
+++ src/components/user/signup.jsx
@@ -1,18 +1,22 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from "../../context/UserContext";
 import { authSignInUp } from "../../services/auth-fetch";
 import UserForm from "./form";
 
 export default function SignUp() {
-  const navigate = useNavigate(); // Utilisez le hook useNavigate
+  const navigate = useNavigate();
+  const { setIsAuthenticated, setUserType } = useContext(UserContext);
 
   const handleSubmit = async ({ username, email, password }) => {
     try {
       const response = await authSignInUp("signup", { username, email, password });
       if (response.ok) {
-        // Redirection après inscription réussie
+        const userData = await response.json();
+        setIsAuthenticated(true);
+        setUserType(userData.userType); // Assurez-vous que votre API renvoie le type d'utilisateur
         navigate('/home');
       } else {
-        // Affichage d'un message d'erreur si l'inscription échoue
         const errorData = await response.json();
         alert(`Erreur lors de l'inscription : ${errorData.message || 'Erreur inconnue'}`);
       }
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -5,9 +5,10 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userType, setUserType] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <UserContext.Provider value={{ userType, setUserType }}>
+    <UserContext.Provider value={{ userType, setUserType, isAuthenticated, setIsAuthenticated }}>
       {children}
     </UserContext.Provider>
   );
