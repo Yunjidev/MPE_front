d1edcade26a753cee6aab4e4516111e0e823c8ba
MacDuPain <godel.Sacha@gmail.com>
2024-08-02 15:39:12 +0200

    "test"

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './footer.css'; // Import du fichier CSS
+import './footer.css'; 
 
 const Footer = () => {
   return (
     <footer className="footer flex justify-center">
-      <p>© 2024 My Website. All rights reserved.</p>
+        <div className="flex flex-col">
+            <p>Contact</p>
+            <p>F.A.Q</p>
+            <p>L'équipe</p>
+            <p>Prix</p>
+            <p>© 2024 My Website. All rights reserved.</p>
+        </div>
     </footer>
   );
 };
