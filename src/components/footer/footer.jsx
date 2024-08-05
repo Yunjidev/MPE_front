3fe59fda01b31a55e43028e08e94b0041e08e4ea
MacDuPain <godel.Sacha@gmail.com>
2024-08-05 10:50:10 +0200

    Footer v1

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import './footer.css'; 
+import "./footer.css"
+import { BsInstagram, BsWhatsapp, BsFacebook, BsTwitterX } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <footer className="footer flex justify-center">
-        <div className="flex flex-col">
-            <p>Contact</p>
-            <p>F.A.Q</p>
-            <p>L'équipe</p>
-            <p>Prix</p>
-            <p>© 2024 My Website. All rights reserved.</p>
+    <footer className="footer flex justify-between items-center p-4">
+      <div className="flex-1"></div>
+
+      <div className="flex flex-col items-center">
+        <a href="http://localhost:5173/contact" className="footer-link">Contact</a>
+        <p>F.A.Q</p>
+        <a href="http://localhost:5173/team" className="footer-link">L'équipe</a>
+        <p>Prix</p>
+        <p>© 2024 MaPetiteEntreprise. Tout droits réservés.</p>
+      </div>
+
+      <div className="flex flex-col items-end flex-1 mr-5">
+        <p className="mr-3">Suivez-nous sur les réseaux</p>
+        <div className="flex space-x-4 mr-4">
+          <BsInstagram size={24} />
+          <BsWhatsapp size={24} />
+          <BsFacebook size={24} />
+          <BsTwitterX size={24} />
         </div>
+        <p>Conditions générales de vente</p>
+        <p className="mr-2">Politique de confidentialité</p>
+        <p className="mr-9">Mentions légales</p>
+      </div>
     </footer>
   );
 };
