a3aa3832074653a9b3ba5fe1cd7138c9cb479cb2
MacDuPain <godel.Sacha@gmail.com>
2024-08-06 12:59:02 +0200

    réarrangement des champs

--- src/components/user/companyform.jsx
+++ src/components/user/companyform.jsx
@@ -24,62 +24,70 @@ export default function RegisterCompany({ onSubmit }) {
   };
 
   return (
-    <div className="flex items-center justify-center h- bg-neutral-900">
-      <div className="relative border-form-1 group max-w-4xl w-full p-6">
+    <div className="flex items-center justify-center h-screen bg-neutral-900">
+      <div className="relative border-form-1 group max-w-8xl w-full p-6">
         <div className="absolute -top-1 -left-1 -right-1 -bottom-1 rounded-xl bg-gradient-to-b from-violet-400 via-green-200 to-orange-400 shadow-lg transition-transform duration-500 group-hover:scale-101"></div>
         <div className="bg-neutral-900 p-10 rounded-xl shadow-2xl relative z-10 transform transition duration-500 ease-in-out">
           <h2 className="text-white text-center text-2xl mb-5">Création d'entreprise</h2>
-          <form onSubmit={handleSubmit} className="space-y-5 grid grid-cols-2 gap-4">
-            <div className="col-span-2 flex justify-center items-center">
+          <form onSubmit={handleSubmit} className="space-y-5 grid grid-cols-3 gap-4">
+            
+            {/* Image Upload */}
+            <div className="col-span-1 flex justify-center items-center">
               <div className="border border-dashed border-gray-500 p-10 rounded-lg cursor-pointer text-gray-400 hover:bg-gray-800">
                 <FaCloudUploadAlt size="3x" />
                 <p className="mt-2">Cliquer pour ajouter des images</p>
               </div>
             </div>
-            <div className="relative flex items-center">
-              <FaUser className="absolute left-3 text-gray-400" />
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Nom Entreprise"
-                className="w-full pl-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
+            
+            {/* Name, Siret, Contact, Email Fields */}
+            <div className="col-span-2 grid grid-cols-2 gap-4">
+              <div className="relative flex items-center">
+                <FaUser className="absolute left-3 text-gray-400" />
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Nom Entreprise"
+                  className="w-full pl-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
+                />
+              </div>
+              <div className="relative flex items-center">
+                <FaBarcode className="absolute right-3 text-gray-400" />
+                <input
+                  type="text"
+                  id="siret"
+                  value={siret}
+                  onChange={(e) => setSiret(e.target.value)}
+                  placeholder="123 Numéro Siret"
+                  className="w-full pr-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
+                />
+              </div>
+              <div className="relative flex items-center">
+                <FaPhone className="absolute left-3 text-gray-400" />
+                <input
+                  type="text"
+                  id="contact"
+                  value={contact}
+                  onChange={(e) => setContact(e.target.value)}
+                  placeholder="Contact"
+                  className="w-full pl-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
+                />
+              </div>
+              <div className="relative flex items-center">
+                <FaEnvelope className="absolute right-3 text-gray-400" />
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="E-mail"
+                  className="w-full pr-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
+                />
+              </div>
             </div>
-            <div className="relative flex items-center">
-              <FaBarcode className="absolute right-3 text-gray-400" />
-              <input
-                type="text"
-                id="siret"
-                value={siret}
-                onChange={(e) => setSiret(e.target.value)}
-                placeholder="123 Numéro Siret"
-                className="w-full pr-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
-            </div>
-            <div className="relative flex items-center">
-              <FaPhone className="absolute left-3 text-gray-400" />
-              <input
-                type="text"
-                id="contact"
-                value={contact}
-                onChange={(e) => setContact(e.target.value)}
-                placeholder="Contact"
-                className="w-full pl-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
-            </div>
-            <div className="relative flex items-center">
-              <FaEnvelope className="absolute right-3 text-gray-400" />
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="E-mail"
-                className="w-full pr-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
-            </div>
+  
+            {/* Remaining Fields */}
             <div className="relative flex items-center">
               <FaMapMarkerAlt className="absolute left-3 text-gray-400" />
               <input
@@ -124,16 +132,9 @@ export default function RegisterCompany({ onSubmit }) {
                 className="w-full pr-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
               />
             </div>
-            <div className="relative flex items-center">
-              <FaPlus className="absolute right-3 text-gray-400" />
-              <input
-                type="text"
-                id="additional"
-                placeholder="Ajouter"
-                className="w-full pr-10 px-3 py-2 rounded-xl bg-neutral-800 text-white focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
-            </div>
-            <div className="relative flex items-center col-span-2">
+  
+            {/* Dropdown */}
+            <div className="relative flex items-center col-span-3">
               <FaSearch className="absolute left-3 text-gray-400" />
               <select
                 id="activity"
@@ -147,7 +148,9 @@ export default function RegisterCompany({ onSubmit }) {
                 <option value="prestation3">Prestation 3</option>
               </select>
             </div>
-            <div className="relative flex items-center col-span-2">
+  
+            {/* Description */}
+            <div className="relative flex items-center col-span-3">
               <FaPenAlt className="absolute left-3 text-gray-400" />
               <textarea
                 id="description"
@@ -158,14 +161,16 @@ export default function RegisterCompany({ onSubmit }) {
                 rows="4"
               />
             </div>
-            <div className="col-span-2 flex justify-center">
+  
+            {/* Submit Button */}
+            <div className="col-span-3 flex justify-center">
               <Button type="submit">Soumettre</Button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  );
+  );     
 }
 
 RegisterCompany.propTypes = {
