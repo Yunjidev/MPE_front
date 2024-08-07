7ce9bd5638f46e277b301f83f6039181e860bc13
Yunjidev <florian.vancamp@gmail.com>
2024-08-07 11:37:44 +0200

    ajout anim home

--- src/pages/home/HomeEntreprise.jsx
+++ src/pages/home/HomeEntreprise.jsx
@@ -1,12 +1,14 @@
 import Pricing from '../../components/pricing/pricing';
 import Jumbotron from '../../components/home/jumbotron';
 import PresentationEnterprise from '../../components/home/presentationenterprise';
+// import OrigamiAnim from '../../components/OrigamiAnim/OrigamiAnim';
 
 const HomeEnterprise = () => {
   return (
     <>
     <Jumbotron />
     <PresentationEnterprise />
+    {/* <OrigamiAnim /> */}
     <Pricing />
 
     </>
