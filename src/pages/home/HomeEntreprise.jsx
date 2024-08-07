8ba12196d5cb3c1734ab7c25a4d8645111b89929
Yunjidev <florian.vancamp@gmail.com>
2024-08-07 11:38:36 +0200

    cleancode

--- src/pages/home/HomeEntreprise.jsx
+++ src/pages/home/HomeEntreprise.jsx
@@ -1,14 +1,12 @@
 import Pricing from '../../components/pricing/pricing';
 import Jumbotron from '../../components/home/jumbotron';
 import PresentationEnterprise from '../../components/home/presentationenterprise';
-// import OrigamiAnim from '../../components/OrigamiAnim/OrigamiAnim';
 
 const HomeEnterprise = () => {
   return (
     <>
     <Jumbotron />
     <PresentationEnterprise />
-    {/* <OrigamiAnim /> */}
     <Pricing />
 
     </>
