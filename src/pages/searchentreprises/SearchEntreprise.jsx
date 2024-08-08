291a5822b0a2821f2b84f0b52c68f9f8518d41e1
BrvAlexis <brevieralexis@gmail.com>
2024-08-08 14:46:27 +0200

    title orange

--- src/pages/searchentreprises/SearchEntreprise.jsx
+++ src/pages/searchentreprises/SearchEntreprise.jsx
@@ -14,7 +14,7 @@ const SearchEntreprise = () => {
         country: '',
         city: '',
         subscription: false,
-        
+
     });
 
     useEffect(() => {
@@ -59,16 +59,16 @@ const SearchEntreprise = () => {
         });
         if (filteredEnterprises.length === 0) {
             setNoResultsMessage(
-            <div className="alert alert-info mb-4">
-                <span>Le critère ne correspond à aucune entreprise pour le moment</span>
-              </div>
+                <div className="alert alert-info mb-4">
+                    <span>Le critère ne correspond à aucune entreprise pour le moment</span>
+                </div>
             );
-      setSearchResults([]); // Utilisez un tableau vide pour indiquer qu'aucun résultat n'a été trouvé
-    } else {
-      setNoResultsMessage('');
-      setSearchResults(filteredEnterprises);
-    }
-  };
+            setSearchResults([]); // Utilisez un tableau vide pour indiquer qu'aucun résultat n'a été trouvé
+        } else {
+            setNoResultsMessage('');
+            setSearchResults(filteredEnterprises);
+        }
+    };
 
     // Mettez à jour les critères de recherche en fonction des sélections de l'utilisateur
     const updateSearchCriteria = (criteria, value) => {
@@ -79,19 +79,21 @@ const SearchEntreprise = () => {
     const displayEnterprises = searchResults.length > 0 ? searchResults : entreprises;
 
     return (
-        <div className="font-sans flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-center text-2xl sm:text-3xl font-bold mb-4 dark:text-white">Recherchez vos entreprises</h1>
-            <IndexSearchbarEntreprises setSearchCriteria={setSearchCriteria} handleSearch={handleSearch} />
-            {noResultsMessage}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-8 md:gap-12 lg:gap-6">
-                {displayEnterprises.map((entreprise) => {
-                    const entrepriseDisponibilites = disponibilities.filter(d => d.Enterprise_id === entreprise.id);
-                    return (
-                        <IndexCardsEntreprises key={entreprise.id} entreprise={entreprise} disponibilites={entrepriseDisponibilites} />
-                    );
-                })}
+        <section className='py-14'>
+            <div className="font-sans flex flex-col items-center justify-center min-h-screen">
+                <h1 className="text-5xl dark:bg-gradient-to-r dark:from-orange-200 dark:to-orange-400 bg-gradient-to-r from-orange-400 to-orange-800 text-transparent bg-clip-text">Recherchez vos entreprises</h1>
+                <IndexSearchbarEntreprises setSearchCriteria={setSearchCriteria} handleSearch={handleSearch} />
+                {noResultsMessage}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-8 md:gap-12 lg:gap-6">
+                    {displayEnterprises.map((entreprise) => {
+                        const entrepriseDisponibilites = disponibilities.filter(d => d.Enterprise_id === entreprise.id);
+                        return (
+                            <IndexCardsEntreprises key={entreprise.id} entreprise={entreprise} disponibilites={entrepriseDisponibilites} />
+                        );
+                    })}
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
