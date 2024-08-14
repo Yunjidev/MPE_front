6dc7598ee97c8941169c1e205b750dd33bd572d2
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-08-14 10:09:08 +0200

    Minor changes

--- src/components/DashboardUser/User_likes.jsx
+++ src/components/DashboardUser/User_likes.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getData } from "../../services/data-fetch";
 
 const LikesManagement = () => {
   const [favorites, setFavorites] = useState([]);
@@ -33,24 +34,21 @@ const LikesManagement = () => {
       <h3 className="text-lg font-medium mb-4">Mes entreprises favorites</h3>
       <hr className="w-full"></hr>
       <ul>
-        <li className="flex justify-between items-center bg-neutral-800 p-2 rounded-lg">
-          <span>❤️ Coiffeuse du 34</span>
-          <button className="bg-white text-black px-3 rounded-lg hover:bg-gray-300">
-            Voir fiche
-          </button>
-        </li>
-        <li className="flex justify-between items-center bg-neutral-800 p-2 rounded-lg">
-          <span>❤️ Plomberie Gégé</span>
-          <button className="bg-white text-black px-3 rounded-lg hover:bg-gray-300">
-            Voir fiche
-          </button>
-        </li>
-        <li className="flex justify-between items-center bg-neutral-800 p-2 rounded-lg">
-          <span>❤️ Maçonnerie</span>
-          <button className="bg-white text-black px-3 rounded-lg hover:bg-gray-300">
-            Voir fiche
-          </button>
-        </li>
+        {favorites.length > 0 ? (
+          favorites.map((favorite) => (
+            <li
+              key={favorite.id}
+              className="flex justify-between items-center bg-neutral-800 p-2 rounded-lg"
+            >
+              <span>❤️ {enterprises[favorite.Enterprise_id]?.name}</span>
+              <button className="bg-white text-black px-3 rounded-lg hover:bg-gray-300">
+                Voir fiche
+              </button>
+            </li>
+          ))
+        ) : (
+          <li className="text-white">Vous n'avez pas encore de favoris.</li>
+        )}
       </ul>
     </div>
   );
