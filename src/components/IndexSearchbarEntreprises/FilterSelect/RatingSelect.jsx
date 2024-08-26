f44180cda09c56e1234e2951d939e29228838775
BrvAlexis <brevieralexis@gmail.com>
2024-08-26 16:29:10 +0200

    fix rating

--- src/components/IndexSearchbarEntreprises/FilterSelect/RatingSelect.jsx
+++ src/components/IndexSearchbarEntreprises/FilterSelect/RatingSelect.jsx
@@ -1,27 +1,42 @@
 import AsyncSelect from 'react-select/async';
+import Rating from 'react-rating-stars-component';
+
+// Fonction pour afficher les étoiles de notation
+export const renderRatingStars = (ratingValue) => {
+  return (
+    <Rating
+      count={5}
+      value={ratingValue}
+      size={24}
+      activeColor="#ffd700"
+      isHalf={true}
+      edit={false}
+    />
+  );
+};
 
 
-const RatingSelect = ({ selectedRatings, setSelectedRatings, loadOptions, renderRatingStars }) => {
-    return (
-      <AsyncSelect
-        isMulti
-        cacheOptions
-        loadOptions={(inputValue) => loadOptions(inputValue, 'averageRating')}
-        onChange={setSelectedRatings}
-        defaultOptions
-        value={selectedRatings}
-        classNamePrefix="react-select-container"
-        className="react-select-container"
-        placeholder="Notes"
-        noOptionsMessage={() => "Pas de notes disponibles"}
-        loadingMessage={() => "Chargement ..."}
-        formatOptionLabel={(option) => (
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            {renderRatingStars(option.value)}
-            <span style={{ marginLeft: '10px' }}>{option.label}</span>
-          </div>
-        )}
-      />
-    );
-  };
+const RatingSelect = ({ selectedRatings, setSelectedRatings, loadOptions }) => {
+
+  return (
+    <AsyncSelect
+      isMulti
+      cacheOptions
+      loadOptions={(inputValue) => loadOptions(inputValue, 'averageRating')}
+      onChange={setSelectedRatings}
+      defaultOptions
+      value={selectedRatings}
+      classNamePrefix="react-select-container"
+      className="react-select-container"
+      placeholder="Notes"
+      noOptionsMessage={() => "Pas de notes disponibles"}
+      loadingMessage={() => "Chargement ..."}
+      formatOptionLabel={(option) => (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {renderRatingStars(option.value)}
+        </div>
+      )}
+    />
+  );
+};
 export default RatingSelect;
--- src/components/IndexSearchbarEntreprises/IndexSearchbarEntreprises.jsx
+++ src/components/IndexSearchbarEntreprises/IndexSearchbarEntreprises.jsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import { getData } from '../../services/data-fetch';
 import './indexsearchbarentreprises.css';
 import { toast } from "react-toastify";
-import Rating from 'react-rating-stars-component';
 import { TfiTarget } from "react-icons/tfi";
 import CitySelect from './FilterSelect/CitySelect';
 import CountrySelect from './FilterSelect/Countryselect';
 import JobSelect from './FilterSelect/Jobselect';
 import PremiumCheckbox from './FilterSelect/PremiumCheckbox';
 import RatingSelect from './FilterSelect/RatingSelect';
+import { renderRatingStars } from './FilterSelect/RatingSelect';
 import Localisation from './LocalisationFilter/Localisation';
 import { HiSearch } from "react-icons/hi";
+
 
 
 const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
@@ -19,8 +20,8 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
   const [selectedCountries, setSelectedCountries] = useState([]);
   const [selectedCities, setSelectedCities] = useState([]);
   const [selectedRatings, setSelectedRatings] = useState([]);
-  
-// Fonction pour réinitialiser le composant AsyncSelect
+
+  // Fonction pour réinitialiser le composant AsyncSelect
 
 
   // Fonctions de chargement des options pour les AsyncSelect
@@ -29,54 +30,59 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
     try {
       const response = await getData(`enterprises/validate`);
       console.log(`Réponse de l'API pour ${category}:`, response);
-      if (category === 'city') {
-        const uniqueCities = new Set();
-        const options = response
-          .filter(item => item.city && item.city.toLowerCase().includes(inputValue.toLowerCase()))
-          .filter(item => {
-            const cityLowerCase = item.city.toLowerCase();
-            if (!uniqueCities.has(cityLowerCase)) {
-              uniqueCities.add(cityLowerCase);
-              return true;
-            }
-            return false;
-          })
-          .map(item => ({ label: item.city, value: item.id }));
-        return options;
-      }
-
       let options;
+
       switch (category) {
-        case 'job':
+        case 'city': {
+          const uniqueCities = new Set();
           options = response
-          .filter(item => item.job && item.job.name && item.job.name.toLowerCase().includes(inputValue.toLowerCase()))
-          .map(item => ({ label: item.job.name }));
-        break;
-        case 'country':
+            .filter(item => item.city && item.city.toLowerCase().includes(inputValue.toLowerCase()))
+            .filter(item => {
+              const cityLowerCase = item.city.toLowerCase();
+              if (!uniqueCities.has(cityLowerCase)) {
+                uniqueCities.add(cityLowerCase);
+                return true;
+              }
+              return false;
+            })
+            .map(item => ({ label: item.city, value: item.id }));
+          break;
+        }
+        case 'job': {
+          options = response
+            .filter(item => item.job && item.job.name && item.job.name.toLowerCase().includes(inputValue.toLowerCase()))
+            .map(item => ({ label: item.job.name }));
+          break;
+        }
+        case 'country': {
           options = response
             .filter(item => item.country && item.country.name && item.country.name.toLowerCase().includes(inputValue.toLowerCase()))
             .map(item => ({ label: item.country.name, value: item.id }));
           break;
-
-        case 'averageRating':
+        }
+        case 'averageRating': {
           options = response
             .filter(item => item.averageRating && item.averageRating.toString().startsWith(inputValue))
-            .map(item => ({ label: item.averageRating, value: item.averageRating }));
+            .map(item => ({ label: item.averageRating, value: item.averageRating }))
+            .sort((a, b) => b.value - a.value);
           break;
-        default:
+        }
+        default: {
           options = [];
+          break;
+        }
       }
+
       console.log(`Options filtrées pour ${category}:`, options);
       return options;
     } catch (error) {
       console.error(`Erreur lors du chargement des options pour ${category}:`, error);
       toast.error(`Une erreur est survenue lors du chargement des options pour ${category}.`);
-      setError(`Une erreur est survenue lors du chargement des options pour ${category}.`);
+      // Assurez-vous que la fonction setError est définie et utilisée correctement ici
       return [];
     }
   };
 
-  // Fonction de recherche
   const performSearch = async () => {
     try {
       // Supposons que cette fonction récupère toutes les entreprises sans filtrage
@@ -101,8 +107,9 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
         const jobMatch = selectedJobs.length === 0 || selectedJobs.some(job => entreprise.job && entreprise.job.name === job.label);
         const countryMatch = selectedCountries.length === 0 || selectedCountries.some(country => entreprise.country && entreprise.country.name === country.label);
         const cityMatch = selectedCities.length === 0 || selectedCities.some(city => entreprise.city === city.label);
-        const ratingMatch = selectedRatings.length === 0 || selectedRatings.some(rating => entreprise.averageRating && entreprise.averageRating.toString() === rating.label);
-
+        const ratingMatch = selectedRatings.length === 0 || selectedRatings.some(rating => entreprise.averageRating && parseFloat(entreprise.averageRating.toFixed(2)) === parseFloat(rating.label));
+        console.log('Notes moyennes après conversion:', response.map(item => item.averageRating.toFixed(2)));
+        console.log('Étiquettes des notes sélectionnées:', selectedRatings.map(rating => rating.label));
         // Affichage des résultats de la correspondance pour le débogage
         console.log(`Résultats de la correspondance pour l'entreprise ${entreprise.name}:`, {
           jobMatch,
@@ -143,22 +150,6 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
 
 
 
-
-
-  // Fonction pour afficher les étoiles de notation
-  const renderRatingStars = (ratingValue) => {
-    return (
-      <Rating
-        count={5}
-        value={ratingValue}
-        size={24}
-        activeColor="#ffd700"
-        isHalf={true}
-        edit={false}
-      />
-    );
-  };
-
   return (
     <div className="join pt-8 pb-10 flex items-center justify-center">
       {/* Open the modal using document.getElementById('ID').showModal() method */}
@@ -179,10 +170,24 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
           </div>
         </div>
       </dialog>
-      <JobSelect selectedJobs={selectedJobs} setSelectedJobs={setSelectedJobs} loadOptions={loadOptions} />
-      <CountrySelect selectedCountries={selectedCountries} setSelectedCountries={setSelectedCountries} loadOptions={loadOptions} />
-      <CitySelect selectedCities={selectedCities} setSelectedCities={setSelectedCities} loadOptions={loadOptions} />
-      <RatingSelect selectedRatings={selectedRatings} setSelectedRatings={setSelectedRatings} loadOptions={loadOptions} renderRatingStars={renderRatingStars} />
+      <JobSelect
+        selectedJobs={selectedJobs}
+        setSelectedJobs={setSelectedJobs}
+        loadOptions={loadOptions} />
+      <CountrySelect
+        selectedCountries={selectedCountries}
+        setSelectedCountries={setSelectedCountries}
+        loadOptions={loadOptions} />
+      <CitySelect
+        selectedCities={selectedCities}
+        setSelectedCities={setSelectedCities}
+        loadOptions={loadOptions} />
+      <RatingSelect
+        selectedRatings={selectedRatings}
+        setSelectedRatings={setSelectedRatings}
+        loadOptions={loadOptions}
+        renderRatingStars={renderRatingStars} // Passer la fonction en tant que prop
+      />
       <PremiumCheckbox />
       <div className="indicator flex gap-2">
         <button onClick={removeLastSearch} className="custom-btn">Supprimer les critères de recherche</button>
