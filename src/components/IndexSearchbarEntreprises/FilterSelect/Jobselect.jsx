db6a8ffa666d4515b4aa6877d39ee2634bdd8a8b
BrvAlexis <brevieralexis@gmail.com>
2024-08-26 11:52:07 +0200

    fix jobs for multisearch

--- src/components/IndexSearchbarEntreprises/FilterSelect/Jobselect.jsx
+++ src/components/IndexSearchbarEntreprises/FilterSelect/Jobselect.jsx
@@ -1,13 +1,26 @@
 import AsyncSelect from 'react-select/async';
 
 
+
 const JobSelect = ({ selectedJobs, setSelectedJobs, loadOptions }) => {
+
+  // Ajout d'une fonction de transformation pour les options sélectionnées
+  const handleChange = (selectedOptions) => {
+    // Transforme les options sélectionnées en un format approprié avant de mettre à jour l'état
+    const transformedSelectedJobs = selectedOptions.map(option => ({
+      label: option.label,
+      value: option.value || option.label // Utilisez label comme valeur de repli si value n'est pas défini
+    }));
+    setSelectedJobs(transformedSelectedJobs);
+  };
+
     return (
       <AsyncSelect
+        
         isMulti
         cacheOptions
         loadOptions={(inputValue) => loadOptions(inputValue, 'job')}
-        onChange={(selectedOptions) => setSelectedJobs(selectedOptions.map(option => ({ label: option.label })))}
+        onChange={handleChange}
         defaultOptions
         value={selectedJobs}
         classNamePrefix="react-select-container"
--- src/components/IndexSearchbarEntreprises/IndexSearchbarEntreprises.jsx
+++ src/components/IndexSearchbarEntreprises/IndexSearchbarEntreprises.jsx
@@ -19,14 +19,16 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
   const [selectedCountries, setSelectedCountries] = useState([]);
   const [selectedCities, setSelectedCities] = useState([]);
   const [selectedRatings, setSelectedRatings] = useState([]);
+  
+// Fonction pour réinitialiser le composant AsyncSelect
 
 
   // Fonctions de chargement des options pour les AsyncSelect
   const loadOptions = async (inputValue, category) => {
-    // console.log(`Chargement des options pour ${category} avec la valeur saisie:`, inputValue);
+    console.log(`Chargement des options pour ${category} avec la valeur saisie:`, inputValue);
     try {
       const response = await getData(`enterprises/validate`);
-      // console.log(`Réponse de l'API pour ${category}:`, response);
+      console.log(`Réponse de l'API pour ${category}:`, response);
       if (category === 'city') {
         const uniqueCities = new Set();
         const options = response
@@ -47,9 +49,9 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
       switch (category) {
         case 'job':
           options = response
-            .filter(item => item.job && item.job.name && item.job.name.toLowerCase().includes(inputValue.toLowerCase()))
-            .map(item => ({ label: item.job.name, value: item.job.id }));
-          break;
+          .filter(item => item.job && item.job.name && item.job.name.toLowerCase().includes(inputValue.toLowerCase()))
+          .map(item => ({ label: item.job.name }));
+        break;
         case 'country':
           options = response
             .filter(item => item.country && item.country.name && item.country.name.toLowerCase().includes(inputValue.toLowerCase()))
@@ -64,7 +66,7 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
         default:
           options = [];
       }
-      // console.log(`Options filtrées pour ${category}:`, options);
+      console.log(`Options filtrées pour ${category}:`, options);
       return options;
     } catch (error) {
       console.error(`Erreur lors du chargement des options pour ${category}:`, error);
@@ -73,75 +75,75 @@ const IndexSearchbarEntreprises = ({ setSearchResults, resetSearch }) => {
       return [];
     }
   };
-  
+
   // Fonction de recherche
   const performSearch = async () => {
     try {
-        // Supposons que cette fonction récupère toutes les entreprises sans filtrage
-        const response = await getData('enterprises/validate');
+      // Supposons que cette fonction récupère toutes les entreprises sans filtrage
+      const response = await getData('enterprises/validate');
 
-        // Affichage des sélections actuelles pour le débogage
-        // console.log('Sélections actuelles:', {
-        //   jobs: selectedJobs,
-        //   countries: selectedCountries,
-        //   cities: selectedCities,
-        //   ratings: selectedRatings
-        // });
+      // Affichage des sélections actuelles pour le débogage
+      console.log('Sélections actuelles:', {
+        jobs: selectedJobs,
+        countries: selectedCountries,
+        cities: selectedCities,
+        ratings: selectedRatings
+      });
 
-        // // Assurez-vous que les ID correspondent aux valeurs attendues
-        // console.log('Exemple de données reçues pour les jobs:', response.map(item => item.job));
-        // console.log('Exemple de données reçues pour les pays:', response.map(item => item.country));
-        // console.log('Exemple de données reçues pour les villes:', response.map(item => item.city));
-        // console.log('Exemple de données reçues pour les notes:', response.map(item => item.averageRating));
+      // Assurez-vous que les ID correspondent aux valeurs attendues
+      console.log('Exemple de données reçues pour les jobs:', response.map(item => item.job));
+      console.log('Exemple de données reçues pour les pays:', response.map(item => item.country));
+      console.log('Exemple de données reçues pour les villes:', response.map(item => item.city));
+      console.log('Exemple de données reçues pour les notes:', response.map(item => item.averageRating));
 
-        // Filtrage des résultats en fonction des sélections de l'utilisateur
-        const filteredResponse = response.filter(entreprise => {
-            const jobMatch = selectedJobs.length === 0 || selectedJobs.some(job => entreprise.job && entreprise.job.name === job.label);
-            const countryMatch = selectedCountries.length === 0 || selectedCountries.some(country => entreprise.country && entreprise.country.name === country.label);
-            const cityMatch = selectedCities.length === 0 || selectedCities.some(city => entreprise.city === city.label);
-            const ratingMatch = selectedRatings.length === 0 || selectedRatings.some(rating => entreprise.averageRating && entreprise.averageRating.toString() === rating.label);
+      // Filtrage des résultats en fonction des sélections de l'utilisateur
+      const filteredResponse = response.filter(entreprise => {
+        const jobMatch = selectedJobs.length === 0 || selectedJobs.some(job => entreprise.job && entreprise.job.name === job.label);
+        const countryMatch = selectedCountries.length === 0 || selectedCountries.some(country => entreprise.country && entreprise.country.name === country.label);
+        const cityMatch = selectedCities.length === 0 || selectedCities.some(city => entreprise.city === city.label);
+        const ratingMatch = selectedRatings.length === 0 || selectedRatings.some(rating => entreprise.averageRating && entreprise.averageRating.toString() === rating.label);
 
-            // Affichage des résultats de la correspondance pour le débogage
-            // console.log(`Résultats de la correspondance pour l'entreprise ${entreprise.name}:`, {
-            //   jobMatch,
-            //   countryMatch,
-            //   cityMatch,
-            //   ratingMatch
-            // });
-            return jobMatch && countryMatch && cityMatch && ratingMatch;
-        }).map(entreprise => ({
-            ...entreprise,
-            logo: entreprise.logo
-        }));
+        // Affichage des résultats de la correspondance pour le débogage
+        console.log(`Résultats de la correspondance pour l'entreprise ${entreprise.name}:`, {
+          jobMatch,
+          countryMatch,
+          cityMatch,
+          ratingMatch
+        });
+        return jobMatch && countryMatch && cityMatch && ratingMatch;
+      }).map(entreprise => ({
+        ...entreprise,
+        logo: entreprise.logo
+      }));
 
-        // console.log('Filtrés:', filteredResponse);
-        // Mise à jour des résultats de recherche
-        setSearchResults(filteredResponse);
-        // console.log('Recherche effectuée avec les paramètres:', filteredResponse);
-        // Si auncun resultat ne correspond à la recherche, afficher une erreur
-        if (filteredResponse.length === 0) {
-            toast.info('Aucun résultat ne correspond à votre recherche.');
-        }
+      console.log('Filtrés:', filteredResponse);
+      // Mise à jour des résultats de recherche
+      setSearchResults(filteredResponse);
+      console.log('Recherche effectuée avec les paramètres:', filteredResponse);
+      // Si auncun resultat ne correspond à la recherche, afficher une erreur
+      if (filteredResponse.length === 0) {
+        toast.info('Aucun résultat ne correspond à votre recherche.');
+      }
     } catch (error) {
-        console.error('Erreur lors de la recherche:', error);
-        toast.error('Une erreur est survenue lors de la recherche.');
-        setError('Une erreur est survenue lors de la recherche.');
+      console.error('Erreur lors de la recherche:', error);
+      toast.error('Une erreur est survenue lors de la recherche.');
+      setError('Une erreur est survenue lors de la recherche.');
     }
-};
+  };
 
-// Fonction pour annuler la dernière recherche
-const removeLastSearch = () => {
+  // Fonction pour annuler la dernière recherche
+  const removeLastSearch = () => {
     // Réinitialisez l'état local des sélections
     setSelectedJobs([]);
     setSelectedCountries([]);
     setSelectedCities([]);
     setSelectedRatings([]);
     resetSearch();
-};
+  };
 
 
 
-  
+
 
   // Fonction pour afficher les étoiles de notation
   const renderRatingStars = (ratingValue) => {
@@ -161,7 +163,7 @@ const removeLastSearch = () => {
     <div className="join pt-8 pb-10 flex items-center justify-center">
       {/* Open the modal using document.getElementById('ID').showModal() method */}
       <button className="custom-localisation-btn flex items-center justify-center" onClick={() => document.getElementById('my_modal_1').showModal()}>
-        <TfiTarget className="ml-4"/>
+        <TfiTarget className="ml-4" />
         <span>Au tour de moi</span>
       </button>
       <dialog id="my_modal_1" className="modal">
@@ -183,8 +185,8 @@ const removeLastSearch = () => {
       <RatingSelect selectedRatings={selectedRatings} setSelectedRatings={setSelectedRatings} loadOptions={loadOptions} renderRatingStars={renderRatingStars} />
       <PremiumCheckbox />
       <div className="indicator flex gap-2">
-      <button onClick={removeLastSearch} className="custom-btn">Supprimer les critères de recherche</button>
-      <button onClick={performSearch} className="custom-btn perform-search"><HiSearch /></button>
+        <button onClick={removeLastSearch} className="custom-btn">Supprimer les critères de recherche</button>
+        <button onClick={performSearch} className="custom-btn perform-search"><HiSearch /></button>
       </div>
     </div>
   );
