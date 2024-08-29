06431b86a3b57de394d362f6b7f1459b6c993fe1
yeezynico <yeezy77680@gmail.com>
2024-08-29 11:04:46 +0200

    photo-cover

--- src/components/ShowEnterprise/PhotoGallery.jsx
+++ src/components/ShowEnterprise/PhotoGallery.jsx
@@ -3,21 +3,21 @@
 
 const PhotoGallery = ({ photos, openPopup }) => {
   return (
-    <div className="md:w-4/5 flex flex-col space-y-4 lg:mr-6">
+    <div className="md:w-4/5 h-screen flex flex-col space-y-4 lg:mr-6">
       {photos && photos.length > 0 ? (
         <>
           <img
             src={photos[0]}
             alt="Photo principale"
-            className="w-full h-auto max-w-[84%] max-h-[80%] rounded-md cursor-pointer"
+            className="w-full h-3/6 object-cover rounded-md cursor-pointer"
             onClick={() => openPopup(photos[0])}
           />
-          <div className="flex w-full gap-4 grid-cols-2 pr-4">
+          <div className="flex w-full h-2/6 gap-4 grid-cols-2">
             {photos[1] && (
               <img
                 src={photos[1]}
                 alt="Photo secondaire 1"
-                className="w-5/12 h-auto max-w-[50%] max-h-[100%] rounded-md cursor-pointer"
+                className="w-5/12  object-cover rounded-md cursor-pointer"
                 onClick={() => openPopup(photos[1])}
               />
             )}
@@ -25,7 +25,7 @@ const PhotoGallery = ({ photos, openPopup }) => {
               <img
                 src={photos[2]}
                 alt="Photo secondaire 2"
-                className="w-5/12 h-auto max-w-[50%] max-h-[100%] rounded-md cursor-pointer"
+                className="w-7/12  object-cover rounded-md cursor-pointer"
                 onClick={() => openPopup(photos[2])}
               />
             )}
